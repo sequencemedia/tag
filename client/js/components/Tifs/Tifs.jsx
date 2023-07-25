959cb717f56849679fc40182b3fc29b845679625
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-07-25 23:07:34 +0100

    Components

--- client/js/components/Tifs/Tifs.jsx
+++ client/js/components/Tifs/Tifs.jsx
@@ -19,9 +19,50 @@ Type.propTypes = {
   handleChange: PropTypes.func.isRequired
 }
 
-export default function Tifs () {
-  const [type, setType] = useState('png')
+function Tifs ({ type, tifs }) {
   const [selectedIndex, setSelectedIndex] = useState(0)
+  const total = tifs.length
+  const index = Math.min(selectedIndex, total)
+  const lastIndex = Math.max(0, total - 1)
+  const isFirstSelected = index === 0
+  const isLastSelected = index === lastIndex
+  const { _id: id } = tifs[index]
+
+  return (
+    <div className='tifs'>
+      <h2>{index + 1} of {total}</h2>
+      <div className='tif-container'>
+        <div className='reverse'>
+          <button disabled={isFirstSelected} onClick={() => {
+            const now = Math.max(0, index - 1)
+
+            if (now !== index) setSelectedIndex(now)
+          }}>Reverse</button>
+        </div>
+
+        <div className='tif'>
+          <img src={`/api/${id}/${type}`} />
+        </div>
+
+        <div className='forward'>
+          <button disabled={isLastSelected} onClick={() => {
+            const now = Math.min(lastIndex, index + 1)
+
+            if (now !== index) setSelectedIndex(now)
+          }}>Forward</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Tifs.propTypes = {
+  type: PropTypes.string.isRequired,
+  tifs: PropTypes.array.isRequired
+}
+
+export default function TifsGroup () {
+  const [type, setType] = useState('png')
   const {
     isConnected,
     tifs
@@ -29,38 +70,10 @@ export default function Tifs () {
 
   if (isConnected) {
     if (tifs.length) {
-      const index = Math.min(selectedIndex, tifs.length)
-      const { _id: id } = tifs[index]
-      const isFirstSelected = index === 0
-      const isLastSelected = index === tifs.length - 1
-
       return (
-        <div>
+        <div className='tifs-group'>
           <Type type={type} handleChange={setType} />
-          <div className='tifs'>
-            <h2>{index + 1} of {tifs.length}</h2>
-            <div className='tif-container'>
-              <div className='reverse'>
-                <button disabled={isFirstSelected} onClick={() => {
-                  const now = Math.max(0, index - 1)
-
-                  if (now !== index) setSelectedIndex(now)
-                }}>Reverse</button>
-              </div>
-
-              <div className='tif'>
-                <img src={`/api/${id}/${type}`} />
-              </div>
-
-              <div className='forward'>
-                <button disabled={isLastSelected} onClick={() => {
-                  const now = Math.min(tifs.length - 1, index + 1)
-
-                  if (now !== index) setSelectedIndex(now)
-                }}>Forward</button>
-              </div>
-            </div>
-          </div>
+          <Tifs type={type} tifs={tifs} />
         </div>
       )
     }
